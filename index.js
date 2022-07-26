098554f22ebb8316c10608827ae760cf3bd2caaf
Marley Watson <marleykw17@gmail.com>
2022-07-26 11:09:53 -0400

    commit tree 2deca88d1bb4809c2035c2ceaf798c93a409225e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+//function updateHeader(name) {
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = 'Marley is the champion'
+document.body.append(newHeader);
